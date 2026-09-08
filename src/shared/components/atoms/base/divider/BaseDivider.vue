2504ea3c33bc2f59abb0f763d40384454cbb8b36
jinwoo <jjw6654@gmail.com>
2026-09-08 16:18:55 +0900

    feat(shared): 구분선 컴포넌트 BaseDivider 추가

---
+++ src/shared/components/atoms/base/divider/BaseDivider.vue
@@ -0,0 +1,13 @@
+<template>
+  <hr class="base-divider" />
+</template>
+
+<style scoped>
+.base-divider {
+  width: 100%;
+  height: 1px;
+  margin: 0;
+  border: none;
+  background: var(--border, #262626);
+}
+</style>
