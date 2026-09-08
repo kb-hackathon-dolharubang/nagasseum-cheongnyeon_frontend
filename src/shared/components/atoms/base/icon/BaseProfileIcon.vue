191614b0fced4f34105861a0a665f3f41abf0d22
jinwoo <jjw6654@gmail.com>
2026-09-08 16:18:55 +0900

    feat(shared): 프로필 아이콘 컴포넌트 BaseProfileIcon 추가

---
+++ src/shared/components/atoms/base/icon/BaseProfileIcon.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  size: { type: Number, default: 24 },
+})
+</script>
+
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24">
+    <circle cx="12" cy="8" r="4" fill="currentColor" />
+    <path d="M4 21C4 16.5817 7.58172 13 12 13C16.4183 13 20 16.5817 20 21" fill="currentColor" />
+  </svg>
+</template>
